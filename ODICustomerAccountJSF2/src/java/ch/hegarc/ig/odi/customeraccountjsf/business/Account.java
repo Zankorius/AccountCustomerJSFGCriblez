241e9899a610d2c846293ea7b49d3960796390f6
Gary Criblez <g.criblez@gmail.com>
2015-11-02 10:26:11 +0100

    import

---
+++ ODICustomerAccountJSF2/src/java/ch/hegarc/ig/odi/customeraccountjsf/business/Account.java
@@ -0,0 +1,103 @@
+package ch.hegarc.ig.odi.customeraccountjsf.business;
+
+public class Account {
+
+    Customer customer;
+    private String number;
+    private String name;
+    private double balance = 0;
+    private double rate = 0.001;
+
+    /**
+     *
+     * @param number
+     * @param name
+     * @param rate
+     * @param customer
+     */
+    public Account(String number, String name, double rate, Customer customer) {
+        this.number = number;
+        this.name = name;
+        this.rate = rate;
+        this.customer = customer;
+
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void credit(double amount) {
+        this.balance = this.balance + amount;
+
+    }
+
+    /**
+     *
+     * @param amount
+     */
+    public void debit(double amount) {
+        this.balance = this.balance - amount;
+
+    }
+
+    /**
+     *
+     * @param amount
+     * @param source
+     * @param target
+     */
+    public static void transfer(double amount, Account source, Account target) {
+        source.balance = source.balance - amount;
+        target.balance = target.balance + amount;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    /**
+     *
+     * @param number
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    /**
+     *
+     * @param balance
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return this.rate;
+    }
+
+    /**
+     *
+     * @param rate
+     */
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+}
---
+++ ODICustomerAccountJSF2/src/java/ch/hegarc/ig/odi/customeraccountjsf/business/Customer.java
@@ -0,0 +1,91 @@
+package ch.hegarc.ig.odi.customeraccountjsf.business;
+
+import java.util.*;
+
+public class Customer {
+
+	Collection<Account> account;
+	private int number;
+	private String firstName;
+	private String lastName;
+
+	/**
+	 * 
+	 * @param number
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Customer(Integer number, String firstName, String lastName) {
+		this.number = number;
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.account = new ArrayList();
+                        
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public Account getAccountByNumber(String number) {
+                ArrayList<Account> parcours = (ArrayList<Account>) this.account;
+                Account cible = null;
+                Integer i = 0;
+		while (cible == null & i < account.size()){
+                    if (parcours.get(i).getNumber().equals(number)){
+                        cible = parcours.get(i);
+                    }
+        }
+            return cible;
+        
+	}
+
+	/**
+	 * 
+	 * @param number
+	 * @param name
+	 * @param rate
+	 */
+	public void addAccount(String number, String name, double rate) {
+		Account nouveau = new Account(number, name, rate, this);
+                this.account.add(nouveau);
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	/**
+	 * 
+	 * @param firstName
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	/**
+	 * 
+	 * @param lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+        
+
+}
